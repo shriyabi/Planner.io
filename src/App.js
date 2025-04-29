ba4e881fdc8d8b04eb46e1dab0f9cc4a24bba22b
shriyabi <shriyarbiddala@gmail.coom>
2025-04-29 01:41:04 -0400

    revamped

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ function App() {
   const [showCategoryColorPicker, setShowCategoryColorPicker] = useState(false);
   const [selectedTaskId, setSelectedTaskId] = useState(null);
   const [selectedColor, setSelectedColor] = useState('#ffffff');
+  const [timeInput, setTimeInput] = useState('00:00'); // editable input
+  const [secondsLeft, setSecondsLeft] = useState(0);    // actual seconds to count down
+  const [isRunning, setIsRunning] = useState(false);
+  const [isEditing, setIsEditing] = useState(true);
 
 
   const toggleMenu = () => {
@@ -78,49 +82,38 @@ function App() {
 
   const handleSaveColor = () => {
     if (selectedTaskId) {
-      // Save the selected color for the task ID
       setTaskColors((prevColors) => ({
         ...prevColors,
         [selectedTaskId]: selectedColor,
       }));
-      setShowCategoryColorPicker(false); // Close the color picker after saving
+      setShowCategoryColorPicker(false);
     }
   };
 
-
-
-
   const formatDateTimeForApi = () => {
     if (!newTaskDueDate) {
-      // Return null if no date is provided (no due date)
       return null;
     }
-
     if (!newTaskTime) {
-      // If no time is provided, assume no specific time, so we only return the date
       const year = newTaskDueDate.substring(0, 4);
-      const month = parseInt(newTaskDueDate.substring(5, 7), 10) - 1; // Month is 0-indexed
+      const month = parseInt(newTaskDueDate.substring(5, 7), 10) - 1;
       const day = parseInt(newTaskDueDate.substring(8, 10), 10);
 
-      const dueDate = new Date(Date.UTC(year, month, day, 0, 0, 0, 0)); // midnight UTC
+      const dueDate = new Date(Date.UTC(year, month, day, 0, 0, 0, 0));
       return dueDate.toISOString();
     }
-
     const [hoursStr, minutesStr] = newTaskTime.split(' ')[0].split(':');
     let hours = parseInt(hoursStr, 10);
     const minutes = parseInt(minutesStr, 10);
     const ampm = newTaskTime.split(' ')[1];
-
     if (ampm === 'PM' && hours !== 12) {
       hours += 12;
     } else if (ampm === 'AM' && hours === 12) {
-      hours = 0; // Midnight
+      hours = 0;
     }
-
     const year = newTaskDueDate.substring(0, 4);
-    const month = parseInt(newTaskDueDate.substring(5, 7), 10) - 1; // Month is 0-indexed
+    const month = parseInt(newTaskDueDate.substring(5, 7), 10) - 1;
     const day = parseInt(newTaskDueDate.substring(8, 10), 10);
-
     const dueDateWithTime = new Date(Date.UTC(year, month, day, hours, minutes, 0, 0));
     return dueDateWithTime.toISOString();
   };
@@ -190,8 +183,7 @@ function App() {
       });
 
       console.log('Task marked as complete:', response);
-      // Remove the task from the UI
-      setTaskList(prevTaskList => {
+      setTaskList(prevTaskList => { //yeet tasks
         const updatedTaskList = { ...prevTaskList };
         if (updatedTaskList[currentDay]) {
           updatedTaskList[currentDay] = updatedTaskList[currentDay].filter(task => task.id !== taskId);
@@ -268,8 +260,6 @@ function App() {
         tasks.forEach(task => {
           if (task.status !== 'completed') {
             let day = 'No Due Date';
-
-            // Check if the task has a due date
             if (task.due) {
               const dueDateUTC = new Date(task.due);
               const offset = new Date().getTimezoneOffset();
@@ -278,13 +268,9 @@ function App() {
               const localDayName = dueDateLocal.toLocaleDateString('en-US', { weekday: 'long' });
               day = localDayName;
             }
-
-            // Ensure 'No Due Date' is included in the tasksByDay object
             if (!tasksByDay[day]) {
               tasksByDay[day] = [];
             }
-
-            // Push the task under the corresponding day (or 'No Due Date')
             tasksByDay[day].push({ id: task.id, title: task.title, discription: task.discription || '' });
           }
         });
@@ -305,15 +291,51 @@ function App() {
     for (let i = 0; i < 7; i++) {
       orderedDays.push(daysOfWeek[(todayIndex + i) % 7]);
     }
-    orderedDays.push('No Due Date'); // Ensure "No Due Date" is at the end of the list
+    orderedDays.push('No Due Date');
     return orderedDays;
   };
 
+  //timer
+  useEffect(() => {
+    let interval = null;
 
+    if (isRunning && secondsLeft > 0) {
+      interval = setInterval(() => {
+        setSecondsLeft(prev => prev - 1);
+      }, 1000);
+    } else if (secondsLeft === 0 && isRunning) {
+      setIsRunning(false);
+      alert('Time is up! 🎉');
+    }
 
+    return () => clearInterval(interval);
+  }, [isRunning, secondsLeft]);
+
+  const handleInputChange = (e) => {
+    setTimeInput(e.target.value);
+  };
+
+  const handleInputBlur = () => {
+    const [minutes, seconds] = timeInput.split(':').map(str => parseInt(str, 10) || 0);
+    setSecondsLeft(minutes * 60 + seconds);
+    setIsEditing(false);
+  };
+
+  const formatTime = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const seconds = secs % 60;
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  };
+
+  const resetTimer = () => {
+    setIsRunning(false);
+    setIsEditing(true);
+    setTimeInput('00:00');
+    setSecondsLeft(0);
+  };
 
   return (
-    <div className="flex px-5 flex-col relative w-screen h-screen items-center justify-top dark:bg-neutral-900">
+    <div className="flex px-5 flex-col relative w-screen h-screen items-center overflow-hidden justify-top dark:bg-neutral-900">
       <div className="App flex flex-col items-center mt-4 space-y-4">
         {!isSignedIn ? (
           <div className='flex flex-col w-screen h-screen items-center justify-center'>
@@ -363,6 +385,46 @@ function App() {
         )}
       </div>
 
+      <div className="flex flex-col items-center bg-green-100 mt-10 space-y-2 p-4 bg-gray-100 rounded-md shadow-md w-1/4">
+      {isEditing ? (
+        <input
+          type="text"
+          value={timeInput}
+          onChange={handleInputChange}
+          onBlur={handleInputBlur}
+          placeholder="00:00"
+          className="text-5xl font-mono text-center border border-gray-400 rounded px-2 py-1"
+        />
+      ) : (
+        <div
+          onClick={() => setIsEditing(true)}
+          className="text-5xl font-semibold font-mono cursor-pointer"
+        >
+          {formatTime(secondsLeft)}
+        </div>
+      )}
+      <div className="flex space-x-2">
+        <button
+          onClick={() => setIsRunning(true)}
+          disabled={secondsLeft <= 0}
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-3 rounded-full text-xs"
+        >
+          Start
+        </button>
+        <button
+          onClick={() => setIsRunning(false)}
+          className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-3 rounded-full text-xs"
+        >
+          Pause
+        </button>
+        <button
+          onClick={resetTimer}
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded-full text-xs"
+        >
+          Reset
+        </button>
+      </div>
+    </div>
 
 
       {isAddTaskOpen && (
@@ -380,24 +442,6 @@ function App() {
                 value={newTaskTitle}
                 onChange={handleNewTaskTitleChange}
               />
-
-
-              {/* <label className="block text-gray-700 text-sm font-bold mb-1">
-                Category Color:
-              </label>
-              <div className="grid grid-cols-5 gap-1 mb-2">
-                {availableColors.map((color) => (
-                  <button
-                    key={color}
-                    type="button"
-                    className={`w-6 h-6 rounded-full focus:outline-none ${color} ${
-                      newTaskCategoryColor === color ? 'border-2 border-gray-500' : ''
-                    }`}
-                    onClick={() => handleNewTaskCategoryColorChange(color)}
-                  ></button>
-                ))}
-              </div> */}
-
 
 
               <label htmlFor="newTaskDueDate" className="block text-gray-700 text-sm font-bold mb-1">
@@ -462,70 +506,67 @@ function App() {
         {taskList && Object.keys(taskList).length > 0 ? (
           getOrderedDays().map(day => (
             taskList[day] && (
-              <div key={day} className="bg-green-400 rounded-md p-4 shadow-md">
+              <div key={day} className="bg-slate-200 rounded-md p-4 shadow-md">
                 <h3 className="text-lg font-semibold mb-2">{day}</h3>
                 {taskList[day].map((task, index) => (
-                  <div key={index} className="border-2 border-slate-300 hover:bg-slate-50 hover:border-solid border-2 hover:border-black rounded-sm p-2 my-2 mb-1 flex items-center justify-between">
+                  <div key={index} style={{ backgroundColor: taskColors[task.id]?.color || '#ffffff' }} className="hover:bg-slate-100 p-2 rounded-lg shadow shadow-lg shadow-slate-500 my-2 mb-1 flex items-center justify-between flex-row">
                     <div className="flex flex-row">
-                      <p className="ml-3 font-semibold">{task.title}</p>
+                      <button
+                        onClick={() => {
+                          setSelectedTaskId(task.id);
+                          setShowCategoryColorPicker(true);
+                        }}
+                        className="bg-black flex justify-center items-center p-3 font-bold h-[1em] w-[1em] hover:bg-gray-400 text-white rounded-full text-lg focus:outline-none focus:shadow-outline"
+                      >
+                        +
+                      </button>
+                      {showCategoryColorPicker && selectedTaskId === task.id ? (
+                        <div className="flex h-full w-full flex-col ml-3">
+                          <label className="text-sm mb-1">
+                            Category:
+                            <input
+                              type="text"
+                              value={taskCategory[task.id] || ''}
+                              onChange={(e) => handleCategoryChange(e, task.id)}
+                              placeholder="Enter category"
+                              className="mt-1 p-1 border rounded w-full"
+                            />
+                          </label>
 
-                      {/* Display color and category button */}
-                      <div className="flex justify-between items-center">
-                        <div
-                          className="task-color p-2 w-10 h-10 rounded"
-                          style={{ backgroundColor: taskColors[task.id]?.color || '#ffffff' }}
-                        >
-                          {/* Placeholder for the task's color */}
-                        </div>
-
-                        {/* Button to open color picker and category input */}
-                        <button
-                          onClick={() => {
-                            setSelectedTaskId(task.id);
-                            setShowCategoryColorPicker(true); // Show category/color picker when button is clicked
-                          }}
-                          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded-full text-xs focus:outline-none focus:shadow-outline"
-                        >
-                          Set Category/Color
-                        </button>
-                      </div>
-
-                      {/* Show color picker and category input if button clicked */}
-                      {showCategoryColorPicker && selectedTaskId === task.id && (
-                        <div className="category-color-picker mt-2">
-                          <div>
-                            <label>
-                              Category:
-                              <input
-                                type="text"
-                                value={taskCategory[task.id] || ''}
-                                onChange={(e) => handleCategoryChange(e, task.id)}
-                                placeholder="Enter category"
-                                className="p-1 border rounded"
-                              />
-                            </label>
-                          </div>
-
-                          <div>
-                            <label>
-                              Color:
-                              <input
-                                type="color"
-                                value={taskColors[task.id]?.color || '#ffffff'}
-                                onChange={(e) => handleColorChange(e, task.id)}
-                              />
-                            </label>
-                          </div>
+                          <label className="text-sm mb-1 mt-2">
+                            Color:
+                            <input
+                              type="color"
+                              value={taskColors[task.id]?.color || '#ffffff'}
+                              onChange={(e) => handleColorChange(e, task.id)}
+                              className="mt-1 w-8 h-8 border rounded"
+                            />
+                          </label>
 
                           <button
                             onClick={() => handleSaveCategoryColor(task.id)}
-                            className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded-full text-xs mt-2"
+                            className="mt-3 bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded-full text-xs"
                           >
                             Save Category/Color
                           </button>
                         </div>
+                      ) : (
+                        <div className="flex ml-3 flex-col w-full">
+                          <p className="font-semibold">{task.title}</p>
+                          {taskCategory[task.id] && (
+                            <p className="italic text-black-500 text-sm">{taskCategory[task.id]}</p>
+                          )}
+                          
+                        </div>
                       )}
+
+
+
+
                     </div>
+                    {showCategoryColorPicker && selectedTaskId === task.id ? (
+                        null
+                    ) : (
                     <button
                       onClick={() => {
                         const taskListsResponse = gapi.client.tasks.tasklists.list();
@@ -538,17 +579,18 @@ function App() {
                           }
                         });
                       }}
-                      className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-2 rounded-full text-xs focus:outline-none focus:shadow-outline"
+                      className="bg-green-400 hover:bg-green-600 text-black hover:text-white font-bold py-1 px-2 rounded-full text-xs focus:outline-none focus:shadow-outline"
                     >
                       Completed
                     </button>
+                    )}
                   </div>
                 ))}
               </div>
             )
           ))
         ) : (
-          <p className="text-center">No tasks available.</p>
+          <p className="text-center" style={{display: 'none'}}>No tasks available.</p>
         )}
       </div>
 
